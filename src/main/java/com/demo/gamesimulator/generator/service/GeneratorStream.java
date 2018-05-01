bd1c18e4e63b74399ba5240357244dd56fc096e5
Baargav <baargavrag@gmail.com>
2018-04-30 21:01:45 -0400

    Add logic for generator

--- src/main/java/com/demo/gamesimulator/generator/service/GeneratorStream.java
+++ src/main/java/com/demo/gamesimulator/generator/service/GeneratorStream.java
@@ -1,13 +1,7 @@
 package com.demo.gamesimulator.generator.service;
 
-import com.demo.gamesimulator.generator.dao.Game;
-import com.demo.gamesimulator.generator.dao.Generator;
-import com.esotericsoftware.kryo.util.ObjectMap;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
@@ -19,10 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerde;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,25 +21,22 @@ import java.util.Map;
 @EnableKafkaStreams
 @EnableKafka
 public class GeneratorStream {
-
-    @Autowired
-    private ObjectMapper mapper;
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
     public StreamsConfig kStreamsConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "generator-stream");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
         props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
         props.put("bootstrap.servers","localhost:9092");
         return new StreamsConfig(props);
     }
 
     @Bean
-    public KStream<Integer, String> kStream(StreamsBuilder kStreamBuilder) {
-
-        KStream<Integer, String> stream = kStreamBuilder.stream("game-seed1");
-        stream.map((k,v) -> ).flatMapValues().to("generated-value");
+    public KStream<String, Integer> kStream(StreamsBuilder kStreamBuilder) {
+        KStream<String, Integer> stream = kStreamBuilder.stream("game-seed2");
+        stream.map((k, v) -> new KeyValue<>(k, v + 42))
+                .to("generated-value");
         return stream;
     }
 
